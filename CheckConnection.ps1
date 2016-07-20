06408ea67703f3ebbe3a32ae45f2a05bca5eb704
sushatgithub <shinde.sushil@gmail.com>
2016-07-20 19:59:33 +0530

    Update CheckConnection.ps1

--- CheckConnection.ps1
+++ CheckConnection.ps1
@@ -1,6 +1,6 @@
-$strString = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')).IsConnectedToInternet
-If ($strString) {
-	
-}Else {
-	netsh wlan connect name=sankalp
-}
+    $sid=$args[0]
+    $strString = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')).IsConnectedToInternet
+    If ($strString) {	
+    }Else {
+	    netsh wlan connect name=$sid
+    }
