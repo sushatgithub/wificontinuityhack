3b998e76784920fa7db74ae2f6009da2a972b563
sushatgithub <shinde.sushil@gmail.com>
2016-07-20 19:14:29 +0530

    Add files via upload

---
+++ Check Connection.ps1
@@ -0,0 +1,6 @@
+$strString = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]'{DCB00C01-570F-4A9B-8D69-199FDBA5723B}')).IsConnectedToInternet
+If ($strString) {
+	
+}Else {
+	netsh wlan connect name=sankalp
+}
